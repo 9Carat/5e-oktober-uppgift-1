0232e102716b4d07efadc28e2af3abc1a3d89b49
Christoffer Ottestig <otte365@hotmail.com>
2022-10-05 12:51:37 +0200

    Add project files.

---
+++ 5e oktober uppgift 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5e_oktober_uppgift_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ThePower.GetPower(5)); ;
+        }
+    }
+}
---
+++ 5e oktober uppgift 1/ThePower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5e_oktober_uppgift_1
+{
+    internal class ThePower
+    {
+        private int number;
+
+        public ThePower(int number)
+        {
+            this.number = number;  
+        }
+
+        public static double GetPower(int number)
+        {
+            double numSquared = Math.Pow(number, 2); 
+            return numSquared;
+        }
+    }
+}
